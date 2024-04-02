66b054883ebe20295956a84cec8693fde273f016
sourabh0627 <165643841+sourabh0627@users.noreply.github.com>
2024-04-02 13:18:44 +0530

    Add files via upload
    Pattern

---
+++ pattern/patteern 11.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int star=1;
+	    for(int i=1;i<=a;i++){
+	        for(int j=1;j<=star;j++){
+	            
+	                 System.out.print("* "); 
+	            
+	           
+	            
+	        }
+    	                   
+	                   System.out.println();
+	                   star++;
+	       
+	     }
+	     a--;
+	     star=a;
+	      for(int i=1;i<=a;i++){
+	        for(int j=1;j<=star;j++){ 
+	           
+	            System.out.print("* "); 
+	            }
+	            
+	        
+	           
+	                   
+	       
+	       	                   
+	                   System.out.println();
+	                   star--; 
+	                   
+	                   
+	           
+	       
+	            
+	        
+
+	     }
+	     
+	     
+	    }
+	}
---
+++ pattern/pattern 12.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int star=1;
+	    int space=a-1;
+	    for(int i=1;i<=a;i++){
+	        for(int j=1;j<=space;j++){
+	            System.out.print("  ");
+	        }
+	        for(int k=1;k<=star;k++){
+	            
+	            System.out.print(" *");
+	            }
+	            
+	        
+    	                   
+	                   System.out.println();
+	                   star++;
+	                   space--;
+	       
+	     }
+	     a--;
+	     star=a;
+	     space=1;
+	      for(int i=1;i<=a;i++){
+	        for(int j=1;j<=space;j++){
+	            System.out.print("  ");
+	            
+	        }
+	        for(int k=1;k<=star;k++){ 
+	            
+	              System.out.print(" *");  
+	            
+	            
+	        }
+	           
+	                   
+	       
+	       	                   
+	                   System.out.println();
+	                   star--; 
+	                   space++;
+	                   
+	                   
+	           
+	       
+	            
+	        
+
+	     }
+	     
+	     
+	    }
+	}
